113589feaf21b7508dac6e83b9f50b889445d96b
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-26 13:43:00 -0400

    commit tree d4612df7e57df9fff14214515bd7db25a3857ea1

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const num3 = 28
+const num4 = 8
+
+const multiply = (num1 * num2);
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = (num3 % num4);
+
+const max = Math.max(1, 5, 10 ,15 ,20)
